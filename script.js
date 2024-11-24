57a7a4fab8f43ee4f7e0dff056197eec3529eab1
karan punamsing bainade <119426472+Karan-Bainade7@users.noreply.github.com>
2024-11-24 22:06:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//strings learn
+// let myname = "karan";
+// console.log(myname);
+
+
+
+
+// let userInput = prompt("enter your name");
+// let user = "@" + userInput + userInput.length;
+// console.log(user);
+
+
+
+
+let obj = {
+    item: "pen",
+    price: 10,
+}
+let output = `the cost of  ${obj.item} is ${obj.price} rs`
+console.log(output);
+
+
+
+let name = "Karanbainade";
+console.log(name[5]);
